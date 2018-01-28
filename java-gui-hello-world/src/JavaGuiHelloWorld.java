1bbf24e12c4ca17f6c660944d040300a90482809
Michel Keselbrener <michel.keselbrener@ezworld.co.il>
2018-01-28 14:53:29 +0200

    Hello GUI Java: 28/01/2018 14:53

---
+++ java-gui-hello-world/src/JavaGuiHelloWorld.java
@@ -0,0 +1,9 @@
+
+public class JavaGuiHelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  System.out.println("Hello GUI Java: 28/01/2018 14:53");  
+	}
+
+}
