77b67fe39aa134f59d4d6abf7e2349ab0fa1af2a
Michel Keselbrener <michel.keselbrener@ezworld.co.il>
2018-01-29 13:29:17 +0200

    shell.setText("Hello GUI Java:29/01/2018 13:25");

--- java-gui-hello-world/src/JavaGuiHelloWorld.java
+++ java-gui-hello-world/src/JavaGuiHelloWorld.java
@@ -1,9 +1,20 @@
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
 
 public class JavaGuiHelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		  System.out.println("Hello GUI Java: 28/01/2018 14:53");  
+		 //System.out.println("Hello GUI Java: 28/01/2018 14:53");
+
+		  Display display = new Display();
+		  Shell shell = new Shell(display);
+		  shell.setText("Hello GUI Java:29/01/2018 13:25");
+		  shell.open();
+		  while (!shell.isDisposed()) {
+			  if (!display.readAndDispatch()) display.sleep();
+		  }	
+		  display.dispose();
 	}
 
 }
